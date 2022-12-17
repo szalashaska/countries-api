20a8a2d0d80929cce328256d7d531466b5b702e3
szalashaska <kamilpetryniak@gmail.com>
2022-12-17 17:13:41 +0100

    Added styles.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useCallback, useEffect, useState } from "react";
+import { CountriesType } from "./helpers/Types";
+import "./styles.scss";
+
 function App() {
-  return <div>Hello World!</div>;
+  const [countriesData, setCountriesData] = useState<CountriesType[]>([]);
+
+  const handleGetData = useCallback(async () => {
+    const request = await fetch("https://restcountries.com/v3.1/all");
+    const response: CountriesType[] = await request.json();
+    setCountriesData(response);
+    // eslint-disable-next-line
+  }, []);
+
+  console.log(countriesData);
+  useEffect(() => {
+    handleGetData();
+  }, [handleGetData]);
+
+  return (
+    <div>
+      Hello World!
+      <div className="dark-theme">elo</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/helpers/Types.ts
@@ -0,0 +1,4 @@
+export type CountriesType = {
+  region: string;
+  independent: boolean;
+};
